d5bc222eee26a0891b4088e3a0c98979a1444a41
fbehddn <tom0611@naver.com>
2023-11-16 16:37:55 +0900

    Day 1 complete

---
+++ src/Day1/Q2.java
@@ -0,0 +1,14 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class Q2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
---
+++ src/Day1/Q5.java
@@ -0,0 +1,7 @@
+package Day1;
+
+public class Q5 {
+    public static void main(String[] args) {
+        System.out.println("!@#$%^&*(\\'\""+"<>?:;");
+    }
+}
