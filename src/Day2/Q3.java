ac3d6814e8ff097939d506b62aa8aaa95b659d48
fbehddn <tom0611@naver.com>
2023-11-17 22:04:41 +0900

    Day 2 complete(except Q5)

---
+++ src/Day2/Q3.java
@@ -0,0 +1,15 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class Q3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        char[] charArray = a.toCharArray();
+        for (char c : charArray) {
+            System.out.println(c);
+        }
+    }
+}
