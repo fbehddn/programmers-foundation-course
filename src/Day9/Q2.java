f481882836940da74a1429e4a1bf9384965e4b65
fbehddn <tom0611@naver.com>
2023-11-22 17:18:23 +0900

    Day 9 complete

---
+++ src/Day9/Q2.java
@@ -0,0 +1,18 @@
+package Day9;
+
+public class Q2 {
+    public String solution(String[] my_strings, int[][] parts) {
+        String answer = "";
+
+        for (int i = 0; i < my_strings.length; i++) {
+            int s = parts[i][0];
+            int e = parts[i][1];
+
+            for (int j = s; j <= e; j++) {
+                answer += my_strings[i].charAt(j);
+            }
+        }
+
+        return answer;
+    }
+}
