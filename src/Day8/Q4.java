00c1b5ee6a3fac66b9077adf27499849203696e0
fbehddn <tom0611@naver.com>
2023-11-22 18:41:19 +0900

    Day 8 complete

---
+++ src/Day8/Q4.java
@@ -0,0 +1,14 @@
+package Day8;
+
+public class Q4 {
+    public int solution(String number) {
+        int answer = 0;
+
+        for (int i = 0; i < number.length(); i++) {
+            answer+= Integer.parseInt(number.charAt(i)+"");
+        }
+
+        answer %= 9;
+        return answer;
+    }
+}
