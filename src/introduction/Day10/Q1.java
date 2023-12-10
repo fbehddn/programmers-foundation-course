5cd8fda6b2092c6fffe755ea9bb9404b566133ec
fbehddn <tom0611@naver.com>
2023-12-10 20:48:34 +0900

    intro - day10 - Q1

---
+++ src/introduction/Day10/Q1.java
@@ -0,0 +1,16 @@
+package introduction.Day10;
+
+public class Q1 {
+    public int solution(int[] dot) {
+        int x = dot[0];
+        int y = dot[1];
+
+        if (x > 0) {
+            if (y > 0) return 1;
+            else return 4;
+        } else {
+            if(y>0) return 2;
+            else return 3;
+        }
+    }
+}
