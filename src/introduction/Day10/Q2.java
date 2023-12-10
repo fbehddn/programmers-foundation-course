8390dd0fde43069a5a85450fdbb97c7d7df95164
fbehddn <tom0611@naver.com>
2023-12-11 02:23:31 +0900

    intro - day10 - Q2

---
+++ src/introduction/Day10/Q2.java
@@ -0,0 +1,15 @@
+package introduction.Day10;
+
+public class Q2 {
+    public int[][] solution(int[] num_list, int n) {
+        int[][] answer = new int[num_list.length / n][n];
+
+        int idx = 0;
+        for (int i = 0; i < num_list.length/n; i++) {
+            for (int j = 0; j < n; j++) {
+                answer[i][j] = num_list[idx++];
+            }
+        }
+        return answer;
+    }
+}
